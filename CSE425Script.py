661e5dbba378f94e93c70f6bb1e6b2ec666b28e0
Asif Haider Rafi <66828930+asifrafi@users.noreply.github.com>
2023-05-06 23:56:52 +0600

    Updates for all pages
    script was modified to take all pages

--- CSE425Script.py
+++ CSE425Script.py
@@ -4,46 +4,20 @@ import requests
 import re
 import openpyxl
 from openpyxl import load_workbook
+
 link = "https://www.startech.com.bd/laptop-notebook/laptop"
-
-html_texts = requests.get(link).text
-soup = BeautifulSoup(html_texts, 'lxml')
-
-laptop_Names = soup.find_all('div', class_='p-item-details')
-laptop_price = soup.find_all('div', class_='p-item-price')
 
 Laptops = ''
 lapname = []
 lapprice = []
-for ln in laptop_Names:
-    for li in ln.find_all("h4"):
-        name = li.text
-        lapname.append(name)
 
-
-for ln in laptop_price:
-    for li in ln.find_all("span"):
-        price = li.text
-        lapprice.append(price)
-
-link = "https://www.startech.com.bd/laptop-notebook/laptop?page=2"
-html_texts = requests.get(link).text
-soup = BeautifulSoup(html_texts, 'lxml')
-
-for ln in laptop_Names:
-    for li in ln.find_all("h4"):
-        name = li.text
-        lapname.append(name)
-
-
-for ln in laptop_price:
-    for li in ln.find_all("span"):
-        price = li.text
-        lapprice.append(price)
 
 def soups(l):
     html_texts = requests.get(l).text
     soup = BeautifulSoup(html_texts, 'lxml')
+    
+    laptop_Names = soup.find_all('div', class_='p-item-details')
+    laptop_price = soup.find_all('div', class_='p-item-price')
     for ln in laptop_Names:
         for li in ln.find_all("h4"):
             name = li.text
@@ -55,8 +29,31 @@ def soups(l):
             price = li.text
             lapprice.append(price)
 
-
-
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=2"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=3"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=4"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=5"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=6"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=7"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=8"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=9"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=10"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=11"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=12"
+soups(link)
+link= "https://www.startech.com.bd/laptop-notebook/laptop?page=13"
+soups(link)
 
 print(lapname)
 print(lapprice)
