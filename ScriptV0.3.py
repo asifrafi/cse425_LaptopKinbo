8911b7780e27966835857aa9f0b13948333a750b
Asif global <asif.rafi1@northsouth.edu>
2023-05-07 01:14:02 +0600

    TechlandScript

--- ScriptV0.3.py
+++ ScriptV0.3.py
@@ -1,1 +1,19 @@
 #ThisIsForTechland
+from bs4 import BeautifulSoup
+import pymongo
+import requests
+import re
+import openpyxl
+from openpyxl import load_workbook
+
+link = "https://www.techlandbd.com/shop-laptop-computer/brand-laptops"
+
+Laptops = ''
+lapname = []
+lapprice = []
+
+
+html_texts = requests.get(link).text
+soup = BeautifulSoup(html_texts, 'lxml')
+laptop_Names = soup.find_all('div', class_='p-item-details')
+laptop_price = soup.find_all('div', class_='p-item-price')
